291fe834467a15f95924854502c06889527a6ea3
yoshidont-mind <tyoshida6@my.bcit.ca>
2024-02-25 11:40:13 -0800

    ADD constructor, accessors

--- src/Creator.java
+++ src/Creator.java
@@ -1,6 +1,30 @@
 import java.util.ArrayList;
 
 public abstract class Creator {
-    private ArrayList<String> name;
-    private String surname; // must be in name
+    private final ArrayList<String> name;
+    private final String surname; // must be in name
+
+    public Creator(ArrayList<String> name, String surname) {
+        if (name == null) {
+            throw new IllegalArgumentException("name must not be null");
+        } else {
+            this.name = name;
+        }
+
+        if (surname == null || surname.isBlank()) {
+            throw new IllegalArgumentException("surname must not be  null, empty, or whitespace String");
+        } else if (!name.contains(surname)) {
+            throw new IllegalArgumentException("surname must be in name");
+        } else {
+            this.surname = surname;
+        }
+    }
+
+    public ArrayList<String> getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
 }
