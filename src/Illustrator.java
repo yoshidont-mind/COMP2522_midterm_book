c561eab8a8d385cb8d044246922eaa9edbcc2a32
yoshidont-mind <tyoshida6@my.bcit.ca>
2024-02-25 11:40:49 -0800

    ADD constructor, accessors

--- src/Illustrator.java
+++ src/Illustrator.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
 
 public final class Illustrator extends Creator{
+    public Illustrator(ArrayList<String> name, String surname) {
+        super(name, surname);
+    }
 }
