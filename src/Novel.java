4b2e6360b8ed6615dc4ac974ec3c5bc314f84555
yoshidont-mind <tyoshida6@my.bcit.ca>
2024-02-25 11:35:34 -0800

    ADD constructor, accessors

--- src/Novel.java
+++ src/Novel.java
@@ -1,4 +1,22 @@
 public class Novel extends Book{
-    private Author author;
-    private int numberOfChapters;
+    private final Author author;
+    private final int numberOfChapters;
+
+    public Novel(String title, String publisher, int publicationYear, int[] isbnNumber, Author author, int numberOfChapters) {
+        super(title, publisher, publicationYear, isbnNumber);
+        this.author = author;
+        if (numberOfChapters <= 0) {
+            throw new IllegalArgumentException("numberOfChapters must be positive");
+        } else {
+            this.numberOfChapters = numberOfChapters;
+        }
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getNumberOfChapters() {
+        return numberOfChapters;
+    }
 }
